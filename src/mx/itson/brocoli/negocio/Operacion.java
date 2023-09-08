de05fe55642687d377529bd1baac9a056d848600
paola figueroa <cpfh27@gmail.com>
2023-09-08 10:55:33 -0700

    Cambios en el codigo

--- src/mx/itson/brocoli/negocio/Operacion.java
+++ src/mx/itson/brocoli/negocio/Operacion.java
@@ -10,11 +10,7 @@ package mx.itson.brocoli.negocio;
  */
 public class Operacion {
 
-    /**    
-     * Separa una cadena de texto a partir de la expresión guion medio.
-     * @param valor Cadena de texto que será separada a partir de la expresión dada.
-     * @return Array de string, en donde cada posición representa una división de la cadena de texto.
-     */
+    // hola, esto es un cambio
     public String[] separar(String valor){
         String[] elementos = valor.split("-");
         return elementos;
